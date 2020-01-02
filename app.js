a172ade78160af004e2e0137e80dcd200d29b9f6
Anton Semenov <anseme@gmail.com>
2020-01-02 20:11:54 +0100

    Add api loader and btn listener

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //Using https://jsonplaceholder.typicode.com/
+const list = document.getElementById("listaado");
+
+document.getElementById("cargar").addEventListener("click", loadApiData);
+
+function loadApiData() {
+  const xhr = new XMLHttpRequest();
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  xhr.open("GET", url, true);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      showData(JSON.parse(this.response));
+    }
+  };
+  xhr.send();
+}
+
+function showData(json) {
+  console.log(json);
+}
