8dab2f1ad26ff25fb22c16d4926f3f79174ac359
Anton Semenov <anseme@gmail.com>
2020-01-02 20:26:06 +0100

    show loaded content

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Using https://jsonplaceholder.typicode.com/
-const list = document.getElementById("listaado");
+const list = document.getElementById("listado");
 
 document.getElementById("cargar").addEventListener("click", loadApiData);
 
@@ -9,12 +9,27 @@ function loadApiData() {
   xhr.open("GET", url, true);
   xhr.onload = function() {
     if (xhr.status === 200) {
-      showData(JSON.parse(this.response));
+      showData(JSON.parse(this.responseText));
     }
   };
   xhr.send();
 }
 
+function loadApiDataModernWay() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(json => showData(json));
+}
+
 function showData(json) {
-  console.log(json);
+  let content = "<table>";
+  json.forEach(post => {
+    content += `<tr>
+      <td>${post.id}</td>
+      <td>${post.title}</td>
+      <td>${post.body}</td>
+      </tr>`;
+  });
+  content += "</table>";
+  list.innerHTML = content;
 }
